34869cae5ba39168ed45e24526f9cda2e8f3950b
NureAlam68 <nurealam151068@gmail.com>
2024-10-02 20:48:21 +0600

    setinterval

---
+++ interval.js
@@ -0,0 +1,28 @@
+console.log(1)
+console.log(2)
+
+
+// setTimeout
+
+// setTimeout(() => {
+//     console.log(3);
+// }, 2000)
+
+
+console.log(4)
+console.log(5)
+console.log(6)
+
+
+// setInterval
+
+
+let num = 0;
+
+const clockId = setInterval( () => {
+    num++;
+    if(num > 6) {
+        clearInterval(clockId);
+    }
+    console.log(clockId, num);
+}, 2000)
